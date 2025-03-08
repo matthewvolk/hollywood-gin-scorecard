c4fcbf9842712b026d18f03282031ea970c8f88e
Matthew Volk <volkmattj@gmail.com>
2025-03-07 23:50:21 -0600

    fix: set page viewport using static viewport object

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,16 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 
 import "./globals.css";
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+};
 
 export const metadata: Metadata = {
   title: "Hollywood Gin Scorecard",
   description: "A scorecard for Hollywood Gin",
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-    maximumScale: 1,
-  },
 };
 
 export default function RootLayout({
